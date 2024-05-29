cd9c038c1f27c2ab3bcd525d951a77a07fb16d59
Darren Thammavong <dt185184@mac-jwncwhwlfl.corp.ncr.com>
2024-05-29 09:11:07 -0400

    Add new LSPs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tailwindcss", "eslint", "jsonls", "typst_lsp", "terraformls" }
+local servers = { "html", "cssls", "tailwindcss", "eslint", "jsonls", "typst_lsp", "terraformls", "marksman", "tflint" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
